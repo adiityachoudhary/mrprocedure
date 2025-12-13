48201dcc90a30165cc6dd53f480ce934ed557f6c
Aditya Choudhary <caditya965@gmail.com>
2025-12-14 02:03:27 +0530

    added tailwind for offine use

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "mr_calculations.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
